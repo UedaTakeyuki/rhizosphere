c0fc2e0f02dd33abcdab15c902d31eea21b6a6e5
Takeyuki UEDA <ueda@latelierdueda.com>
2018-10-13 18:28:35 +0900

    refactoring

--- sample_handlers/sample_consolehandler.py
+++
@@ -1,7 +0,0 @@
-import tornado.web
-import pprint
-
-class ConsoleHandler(tornado.web.RequestHandler):
-	def get(self):
-#   self.write(pprint.pformat(connections))
-		self.render('index.html', connections=connections)
