90cb1075ec628807ce2f4efe2d6c472c3e221c9b
Takeyuki UEDA <ueda@latelierdueda.com>
2018-10-13 13:03:25 +0900

    refactoring

--- config.py
+++ config.py
@@ -1,7 +1,12 @@
 protocol = "wss:"
-devices_module  = "sample_deviceshandler"
+
+device_module  = "sample_deviceshandler"
+webportalpage_module = "sample_consolehandler"
+webcommandpage_module = "sample_commandhandler"
+client_module = "sample_clienthandler"
+
 data_dir = "/etc/letsencrypt/live/titurel.uedasoft.com/"
 additional_module_path    = ["test", "sample_handlers"] 
 #rhizome_module_name  = "a"
-devices_handler = "WebSocketHandler"
-devices_route = "/websocket"
+device_handler = "WebSocketHandler"
+device_route = "/websocket"
--- main.py
+++ main.py
@@ -32,12 +32,63 @@ if __name__ == "__main__":
     define("cert_file",             default="cert.pem", help="cert file name for running with ssl")
     define("privkey_file",          default="privkey.pem", help="privkey file name for running with ssl")
     define("config_file",           default="",         help="config file path")
+
+###############
+# Handlers 
+###############
+    ''' 
+      DeviceHandler:
+
+      Feature:
+        Make a bi-directional connection between rhizosphere and device, according to the hand shake request from device.
+        Then wait message from rhizospere and return response from device.
+
+    '''
+    define("device_route",         default="/device", help="route of request to make a connection with device, and add it to rhizospere.", metavar="/route")
+    define("device_module",        default="",        help="[mandatory] module name of rhizome")
+    define("device_handler",       default="",        help="[mandatory] handler class name of rhizome")
+
+    ''' 
+      WebPortalPageHander:
+
+      Feature:
+        Provide portal web interface.
+      
+    '''
+    define("webportalpage_route",         default="/",        help="route of devices",metavar="/route")
+    define("webportalpage_module",        default="",         help="[mandatory] module name of rhizome")
+    define("webportalpage_handler",       default="",         help="[mandatory] handler class name of rhizome")
+
+    ''' 
+      WebCommandPageHandler:
+
+      Feature:
+        Provide command sending UI as web.
+      
+    '''
+    define("webcommandpage_route",         default="/command", help="route of devices",metavar="/route")
+    define("webcommandpage_module",        default="",         help="[mandatory] module name of rhizome")
+    define("webcommandpage_handler",       default="",         help="[mandatory] handler class name of rhizome")
+
+    ''' 
+      ClientHandler:
+
+      Feature:
+        Make a bi-directional connection between rhizosphere and client, according to the hand shake request from client.
+        Then wait message from rhizospere and return response from client.
+
+    '''
+    define("client_route",         default="/client/(.*)", help="route of request to make a connection between client and device specified by id, and add it to rhizospere.",metavar="/device/*device_id*")
+    define("client_module",        default="",         help="[mandatory] module name of rhizome")
+    define("client_handler",       default="",         help="[mandatory] handler class name of rhizome")
+
+###############
+# Path 
+###############
     define("additional_module_path",default="",         help="path of importing modules",multiple=True, metavar="path1, path2...")
-    define("devices_route",         default="/devices", help="route of devices",metavar="/route")
-    define("devices_module",        default="",         help="[mandatory] module name of rhizome")
-    define("devices_handler",       default="",         help="[mandatory] handler class name of rhizome")
     define("static_path",           default="sample_handlers/static",        help="[mandatory] handler class name of rhizome")
     define("templates_path",        default="sample_handlers/templates",     help="[mandatory] handler class name of rhizome")
+
     options.parse_command_line()
     if options.config_file:
         options.parse_config_file(options.config_file)
@@ -50,20 +101,20 @@ if __name__ == "__main__":
     if options.additional_module_path:
         [sys.path.append(x) for x in options.additional_module_path]
 #        sys.path.append(options.rhizome_module_path)
-    mod_devs = importlib.import_module(options.devices_module)
+    mod_devs = importlib.import_module(options.device_module)
 #    mod_devs.cl = cl
     mod_devs.connections = connections
-    deviceshandler = getattr(mod_devs, options.devices_handler)
+    deviceshandler = getattr(mod_devs, options.device_handler)
 
     mod_cons = importlib.import_module("sample_consolehandler")
 #    mod_cons.cl = cl
     mod_cons.connections = connections
-    consolehandler = getattr(mod_cons, "ConsoleHandler")
+    webportalpagehandler = getattr(mod_cons, "ConsoleHandler")
 
     mod_cmd = importlib.import_module("sample_commandhandler")
 #    mod_cmd.cl = cl
     mod_cmd.connections = connections
-    commandhandler = getattr(mod_cmd, "CommandHandler")
+    webcommandpagehandler = getattr(mod_cmd, "CommandHandler")
 
     mod_clt = importlib.import_module("sample_clienthandler")
 #    mod_clt.cl = cl
@@ -74,10 +125,13 @@ if __name__ == "__main__":
 # app
     BASE_DIR = os.path.dirname(__file__)
     app = tornado.web.Application([
-            (options.devices_route, deviceshandler),
-            (r"/console",   consolehandler),
-            (r"/command",   commandhandler),
-            (r"/client/(.*)",    clienthandler),            
+            (options.device_route,          deviceshandler),
+            (options.webportalpage_route,   webportalpagehandler),
+            (options.webcommandpage_route,  webcommandpagehandler),
+            (options.client_route,          clienthandler),            
+#            (r"/console",   consolehandler),
+#            (r"/command",   commandhandler),
+#            (r"/client/(.*)",    clienthandler),            
         ],
         template_path=os.path.join(BASE_DIR, options.templates_path),
         static_path=os.path.join(BASE_DIR, options.static_path),
